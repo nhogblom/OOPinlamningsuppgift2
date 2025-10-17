60e4608759189a085f0abd5fbdfb45a699a8e85c
Niklas Högblom <nhogblom@gmail.com>
2025-10-17 12:54:55 +0200

    Created Class Person

---
+++ src/Person.java
@@ -0,0 +1,11 @@
+public abstract class Person {
+    private String name;
+    private String adress;
+    private String personnummer;
+
+    public Person(String name, String adress, String personnummer) {
+        this.name = name;
+        this.adress = adress;
+        this.personnummer = personnummer;
+    }
+}
