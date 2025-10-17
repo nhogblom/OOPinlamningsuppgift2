b9294341a1a7fd9fb38cb099c231f4e13125e75e
Niklas Högblom <nhogblom@gmail.com>
2025-10-17 13:45:19 +0200

    Done with test for CreateMemberFromFileData

--- src/RecoverFromFile.java
+++ src/RecoverFromFile.java
@@ -1,3 +1,4 @@
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -5,8 +6,40 @@ import java.util.Scanner;
 public class RecoverFromFile {
 
 
+    public static Member createMemberFromFileData(String[] rawMemberData) {
+        Member member = null;
+        try {
+            String name = rawMemberData[0].trim();
+            String adress = rawMemberData[1].trim();
+            String email = rawMemberData[2].trim();
+            String personnummer = rawMemberData[3].trim();
+            LocalDate dateOfBecommingMember = LocalDate.parse(rawMemberData[4].trim());
+            LocalDate dateOfMostRecentMembershipRenewal = LocalDate.parse(rawMemberData[5].trim());
+            String memberTypeString = rawMemberData[6].trim();
+            memberType memberTypeCurrent = null;
+            for (memberType mType : memberType.values()) {
+                if (mType.name().equalsIgnoreCase(memberTypeString)) {
+                    memberTypeCurrent = mType;
+                }
+            }
+            member = new Member(name, adress, personnummer, email, dateOfBecommingMember, dateOfMostRecentMembershipRenewal, memberTypeCurrent);
+        } catch (Exception e) {
+            System.out.println("Fel vid inläsning av användardata.."+e.getMessage());
+
+        }
+        return member;
+    }
+
     public static List<Member> readMembersFromFileToList(Scanner sc) {
         List<Member> members = new ArrayList<>();
+        String line;
+
+        while ((line = sc.nextLine()) != null) {
+            String[] memberValuesSeparated = new String[7];
+            memberValuesSeparated = line.split(";");
+
+            //String name =
+        }
         return members;
     }
 }
--- src/RecoverFromFileTest.java
+++ src/RecoverFromFileTest.java
@@ -1,6 +1,5 @@
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -10,7 +9,22 @@ class RecoverTool {
     List<Member> membersList;
 
     @Test
-    void readMembersFromFileTest(){
+    void createMemberFromFileDataTest() {
+        String[] rawMemberData = {"Fredrik Berggren", "Skolgränd 8, 16819 Norrköping", "fredde@fakemail.se", "851020-6728", "2019-12-30", "2021-12-30","Platina"};
+        String expectedMemberName = "Fredrik Berggren";
+        String expectedMemberPersonnummer = "851020-6728";
+        String unexpectedMemberName = "Fredrik Johansson";
+        String unexpectedMemberPersonnummer = "890101-1234";
+
+        assertEquals(expectedMemberName, RecoverFromFile.createMemberFromFileData(rawMemberData).getName());
+        assertEquals(expectedMemberPersonnummer, RecoverFromFile.createMemberFromFileData(rawMemberData).getPersonnummer());
+        assertNotEquals(unexpectedMemberName, RecoverFromFile.createMemberFromFileData(rawMemberData).getName());
+        assertNotEquals(unexpectedMemberPersonnummer, RecoverFromFile.createMemberFromFileData(rawMemberData).getPersonnummer());
+
+    }
+
+    @Test
+    void readMembersFromFileTest() {
         String exampleRecoveryFileTextForTwoUsers = "Namn;Adress;Mailadress;Personnummer;Datum_köpt_gymmedlemskap;Datum_senast_uppdaterad;Medlemsnivå\n" +
                 "Fredrik Berggren;Skolgränd 8, 16819 Norrköping;fredde@fakemail.se;851020-6728;2019-12-30;2021-12-30;Platina\n" +
                 "Astrid Larsson;Järnvägsvägen 5, 64230 Gävle;asta@fakemail.de;540815-4382;2021-12-04;2022-12-04;Platina";
@@ -18,10 +32,9 @@ class RecoverTool {
 
         membersList = RecoverFromFile.readMembersFromFileToList(sc);
 
-        assertEquals(2,membersList.size());
-        assertNotEquals(1,membersList.size());
-        assertNotEquals(3,membersList.size());
+        assertEquals(2, membersList.size());
+        assertNotEquals(1, membersList.size());
+        assertNotEquals(3, membersList.size());
 
     }
-
 }
