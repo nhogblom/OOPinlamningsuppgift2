36bc4ecf50844529218a767e7704203b49caf34e
Niklas Högblom <nhogblom@gmail.com>
2025-10-17 13:08:58 +0200

    Small changes in preparation of TDD

--- src/RecoverFromFile.java
+++ src/RecoverFromFile.java
@@ -1,4 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class RecoverFromFile {
 
 
+    public static List<Member> readMembersFromFileToList(Scanner sc) {
+        List<Member> members = new ArrayList<>();
+        return members;
+    }
 }
